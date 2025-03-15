88d9318241b7cbae7df07488b6126f647202ef7b
xxxmetalin <xxxkochoran@gmail.com>
2025-03-15 21:34:03 +0900

    Initial commit for refe01-3

--- ヒラックス デイサービスFC _ 説明会参加でPayPayポイント5万円分もらえる_files/main.js
+++ ヒラックス デイサービスFC _ 説明会参加でPayPayポイント5万円分もらえる_files/main.js
@@ -1,9 +1,21 @@
+import './style.css'
+import 'slick-carousel'
+import jQuery from 'jquery'
+
+window.$ = jQuery
+
 $(document).ready(function(){
-    $('.slider').slick({
+  $('.slider').slick({
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
-    slidesToScroll: 1
-    });
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: false,
+    cssEase: 'ease',
+    prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+    nextArrow: '<button type="button" class="slick-next">&#10095;</button>'
   });
+});
